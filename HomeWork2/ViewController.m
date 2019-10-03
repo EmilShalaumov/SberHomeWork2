66a531f03c7d6f31b41e6e2dbfca4bd21b0fb6ae
Emil Shalaumov <emil.shalaumov@gmail.com>
2019-10-03 21:48:56 +0300

    Initial Commit

---
+++ HomeWork2/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  HomeWork2
+//
+//  Created by Эмиль Шалаумов on 03/10/2019.
+//  Copyright © 2019 Emil Shalaumov. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
