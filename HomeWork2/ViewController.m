8106dcf8e54349b80a519346ccc205bb51f283d5
Emil Shalaumov <emil.shalaumov@gmail.com>
2019-10-05 11:56:26 +0300

    Delegates implementation

--- HomeWork2/ViewController.m
+++ HomeWork2/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "ISSGuest.h"
 
 @interface ViewController ()
 
@@ -17,6 +18,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+
+    ISSGuest *guest = [[ISSGuest alloc] init];
+    
+    [guest makeOrder];
 }
 
 
